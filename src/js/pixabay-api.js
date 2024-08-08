373721c1531edacf75dd4cc3e472c17133da1bdd
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-08-08 20:15:51 +0300

    ів

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API_KEY = '45178641-894ab33b0f7791a2b3522c7a2';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImages(query, page = 1) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: 15,
+    page: page,
+  };
+
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
